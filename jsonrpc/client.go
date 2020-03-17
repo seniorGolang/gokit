727230037ecf2e540f70de7395625af239e563b8
AlexK <contact@altsoftllc.com>
2020-03-17 17:34:16 +1000

    add jsonRPC

---
+++ jsonrpc/client.go
@@ -0,0 +1,247 @@
+package jsonrpc
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+
+	"bitbucket.org/ronte/msg-gokit-packages/types/uuid"
+	"github.com/go-kit/kit/endpoint"
+	httpTransport "github.com/go-kit/kit/transport/http"
+)
+
+// Client wraps a JSON RPC method and provides a method that implements endpoint.Endpoint.
+type Client struct {
+	client httpTransport.HTTPClient
+
+	// JSON RPC endpoint URL
+	tgt *url.URL
+
+	// JSON RPC method name.
+	method string
+
+	enc            EncodeRequestFunc
+	dec            DecodeResponseFunc
+	before         []httpTransport.RequestFunc
+	after          []httpTransport.ClientResponseFunc
+	finalizer      httpTransport.ClientFinalizerFunc
+	requestID      RequestIDGenerator
+	bufferedStream bool
+}
+
+// NewClient constructs a usable Client for a single remote method.
+func NewClient(
+	tgt *url.URL,
+	method string,
+	options ...ClientOption,
+) *Client {
+	c := &Client{
+		client:         http.DefaultClient,
+		method:         method,
+		tgt:            tgt,
+		enc:            DefaultRequestEncoder,
+		dec:            DefaultResponseDecoder,
+		before:         []httpTransport.RequestFunc{},
+		after:          []httpTransport.ClientResponseFunc{},
+		requestID:      NewUUIDGenerator(),
+		bufferedStream: false,
+	}
+	for _, option := range options {
+		option(c)
+	}
+	return c
+}
+
+// DefaultRequestEncoder marshals the given request to JSON.
+func DefaultRequestEncoder(_ context.Context, req interface{}) (json.RawMessage, error) {
+	return json.Marshal(req)
+}
+
+// DefaultResponseDecoder unmarshals the result to interface{}, or returns an
+// error, if found.
+func DefaultResponseDecoder(_ context.Context, res Response) (interface{}, error) {
+	if res.Error != nil {
+		return nil, *res.Error
+	}
+	var result interface{}
+	err := json.Unmarshal(res.Result, &result)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+// ClientOption sets an optional parameter for clients.
+type ClientOption func(*Client)
+
+// SetClient sets the underlying HTTP client used for requests.
+// By default, http.DefaultClient is used.
+func SetClient(client httpTransport.HTTPClient) ClientOption {
+	return func(c *Client) { c.client = client }
+}
+
+// ClientBefore sets the RequestFuncs that are applied to the outgoing HTTP
+// request before it's invoked.
+func ClientBefore(before ...httpTransport.RequestFunc) ClientOption {
+	return func(c *Client) { c.before = append(c.before, before...) }
+}
+
+// ClientAfter sets the ClientResponseFuncs applied to the server's HTTP
+// response prior to it being decoded. This is useful for obtaining anything
+// from the response and adding onto the context prior to decoding.
+func ClientAfter(after ...httpTransport.ClientResponseFunc) ClientOption {
+	return func(c *Client) { c.after = append(c.after, after...) }
+}
+
+// ClientFinalizer is executed at the end of every HTTP request.
+// By default, no finalizer is registered.
+func ClientFinalizer(f httpTransport.ClientFinalizerFunc) ClientOption {
+	return func(c *Client) { c.finalizer = f }
+}
+
+// ClientRequestEncoder sets the func used to encode the request params to JSON.
+// If not set, DefaultRequestEncoder is used.
+func ClientRequestEncoder(enc EncodeRequestFunc) ClientOption {
+	return func(c *Client) { c.enc = enc }
+}
+
+// ClientResponseDecoder sets the func used to decode the response params from
+// JSON. If not set, DefaultResponseDecoder is used.
+func ClientResponseDecoder(dec DecodeResponseFunc) ClientOption {
+	return func(c *Client) { c.dec = dec }
+}
+
+// RequestIDGenerator returns an ID for the request.
+type RequestIDGenerator interface {
+	Generate() *RequestID
+}
+
+// ClientRequestIDGenerator is executed before each request to generate an ID
+// for the request.
+// By default, AutoIncrementRequestID is used.
+func ClientRequestIDGenerator(g RequestIDGenerator) ClientOption {
+	return func(c *Client) { c.requestID = g }
+}
+
+// BufferedStream sets whether the Response.Body is left open, allowing it
+// to be read from later. Useful for transporting a file as a buffered stream.
+func BufferedStream(buffered bool) ClientOption {
+	return func(c *Client) { c.bufferedStream = buffered }
+}
+
+// Endpoint returns a usable endpoint that invokes the remote endpoint.
+func (c Client) Endpoint() endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		ctx, cancel := context.WithCancel(ctx)
+		defer cancel()
+
+		var (
+			resp *http.Response
+			err  error
+		)
+		if c.finalizer != nil {
+			defer func() {
+				if resp != nil {
+					ctx = context.WithValue(ctx, httpTransport.ContextKeyResponseHeaders, resp.Header)
+					ctx = context.WithValue(ctx, httpTransport.ContextKeyResponseSize, resp.ContentLength)
+				}
+				c.finalizer(ctx, err)
+			}()
+		}
+
+		var params json.RawMessage
+		if params, err = c.enc(ctx, request); err != nil {
+			return nil, err
+		}
+		rpcReq := Request{
+			JSONRPC: "",
+			Method:  c.method,
+			Params:  params,
+			ID:      c.requestID.Generate(),
+		}
+
+		req, err := http.NewRequest("POST", c.tgt.String()+"/"+c.method, nil)
+		if err != nil {
+			return nil, err
+		}
+
+		req.Header.Set("Content-Type", "application/json; charset=utf-8")
+		var b bytes.Buffer
+		req.Body = ioutil.NopCloser(&b)
+		err = json.NewEncoder(&b).Encode(rpcReq)
+		if err != nil {
+			return nil, err
+		}
+
+		for _, f := range c.before {
+			ctx = f(ctx, req)
+		}
+
+		resp, err = c.client.Do(req.WithContext(ctx))
+		if err != nil {
+			return nil, err
+		}
+
+		if !c.bufferedStream {
+			defer resp.Body.Close()
+		}
+
+		// Decode the body into an object
+		var rpcRes Response
+		err = json.NewDecoder(resp.Body).Decode(&rpcRes)
+		if err != nil {
+			return nil, err
+		}
+
+		for _, f := range c.after {
+			ctx = f(ctx, resp)
+		}
+
+		return c.dec(ctx, rpcRes)
+	}
+}
+
+// SetAuthorizaedHeader returns a RequestFunc that set Authorization header
+func SetAuthorizationHeader(token string) httpTransport.RequestFunc {
+	return func(ctx context.Context, r *http.Request) context.Context {
+		var bearer = "Bearer " + token
+		r.Header.Set(http.CanonicalHeaderKey("Authorization"), bearer)
+		return ctx
+	}
+}
+
+// SetUserIDHeader returns a RequestFunc that set X-User-Id header
+func SetUserIDHeader(userID uuid.UUID) httpTransport.RequestFunc {
+	return func(ctx context.Context, r *http.Request) context.Context {
+		r.Header.Set(http.CanonicalHeaderKey("X-User-Id"), userID.String())
+		return ctx
+	}
+}
+
+// SetAuthorizedHeader returns a RequestFunc that sets Authorization header
+func SetSessionIDHeader(sessionID uuid.UUID) httpTransport.RequestFunc {
+	return func(ctx context.Context, r *http.Request) context.Context {
+		r.Header.Set(http.CanonicalHeaderKey("X-Session-Id"), sessionID.String())
+		return ctx
+	}
+}
+
+// autoUUIDGeneratorID is a RequestIDGenerator that generates
+// new uuid v4
+type autoUUIDGeneratorID struct{}
+
+// NewUUIDGenerator returns an auto-incrementing request ID generator,
+// initialised with the given value.
+func NewUUIDGenerator() RequestIDGenerator {
+	return &autoUUIDGeneratorID{}
+}
+
+// Generate satisfies RequestIDGenerator
+func (i *autoUUIDGeneratorID) Generate() *RequestID {
+	return &RequestID{
+		stringValue: uuid.NewV4().String(),
+	}
+}
---
+++ jsonrpc/server.go
@@ -0,0 +1,223 @@
+package jsonrpc
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	httpTransport "github.com/go-kit/kit/transport/http"
+	"github.com/gorilla/mux"
+)
+
+const (
+	reqID = "requestID"
+)
+
+// Server wraps an endpoint and implements http.Handler.
+type Server struct {
+	ecm          EndpointCodecMap
+	errorEncoder httpTransport.ErrorEncoder
+	before       []httpTransport.RequestFunc
+	finalizer    httpTransport.ServerFinalizerFunc
+	after        []httpTransport.ServerResponseFunc
+}
+
+// NewServer constructs a new server, which implements http.Server.
+func NewServer(ecm EndpointCodecMap, options ...ServerOption) *Server {
+	s := &Server{
+		ecm:          ecm,
+		errorEncoder: DefaultErrorEncoder,
+	}
+	for _, option := range options {
+		option(s)
+	}
+	return s
+}
+
+// ServerOption sets an optional parameter for servers.
+type ServerOption func(*Server)
+
+// ServerBefore functions are executed on the HTTP request object before the
+// request is decoded.
+func ServerBefore(before ...httpTransport.RequestFunc) ServerOption {
+	return func(s *Server) { s.before = append(s.before, before...) }
+}
+
+// ServerAfter functions are executed on the HTTP response writer after the
+// endpoint is invoked, but before anything is written to the client.
+func ServerAfter(after ...httpTransport.ServerResponseFunc) ServerOption {
+	return func(s *Server) { s.after = append(s.after, after...) }
+}
+
+// ServerErrorEncoder is used to encode errors to the http.ResponseWriter
+// whenever they're encountered in the processing of a request. Clients can
+// use this to provide custom error formatting and response codes. By default,
+// errors will be written with the DefaultErrorEncoder.
+func ServerErrorEncoder(ee httpTransport.ErrorEncoder) ServerOption {
+	return func(s *Server) { s.errorEncoder = ee }
+}
+
+// ServeHTTP implements http.Handler.
+func (s Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodPost {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		_, _ = io.WriteString(w, "405 must POST\n")
+		return
+	}
+
+	ctx := r.Context()
+
+	for _, f := range s.before {
+		ctx = f(ctx, r)
+	}
+
+	// Decode the body into an  object
+	var req Request
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		rpcErr := parseError("JSON could not be decoded: " + err.Error())
+		s.errorEncoder(ctx, rpcErr, w)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	var ok bool
+	var method string
+	vars := mux.Vars(r)
+
+	if method, ok = vars["method"]; !ok && req.Method == "" {
+
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusNotFound)
+		_, _ = io.WriteString(w, fmt.Sprintf("404 unknown method '%s'\n", method))
+		return
+
+	} else if req.Method != "" && req.Method != method {
+
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusBadRequest)
+		_, _ = io.WriteString(w, fmt.Sprintf("400 incorrect method '%s != %s'\n", method, req.Method))
+		return
+
+	} else {
+		req.Method = method
+	}
+
+	ctx = context.WithValue(ctx, reqID, req.ID)
+
+	// Get the endpoint and codecs from the map using the method
+	// defined in the JSON  object
+	ecm, ok := s.ecm[req.Method]
+	if !ok {
+		err := methodNotFoundError(fmt.Sprintf("Method %s was not found.", req.Method))
+		s.errorEncoder(ctx, err, w)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// Decode the JSON "params"
+	reqParams, err := ecm.Decode(ctx, req.Params)
+	if err != nil {
+		s.errorEncoder(ctx, err, w)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// Call the Endpoint with the params
+	response, err := ecm.Endpoint(ctx, reqParams)
+	if err != nil {
+		s.errorEncoder(ctx, err, w)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	for _, f := range s.after {
+		ctx = f(ctx, w)
+	}
+
+	res := Response{
+		ID:      req.ID,
+		JSONRPC: Version,
+	}
+
+	// Encode the response from the Endpoint
+	resParams, err := ecm.Encode(ctx, response)
+	if err != nil {
+		s.errorEncoder(ctx, err, w)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	res.Result = resParams
+
+	w.Header().Set("Content-Type", ContentType)
+
+	if err := json.NewEncoder(w).Encode(res); err != nil {
+		s.errorEncoder(ctx, err, w)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+// DefaultErrorEncoder writes the error to the ResponseWriter,
+// as a json-rpc error response, with an InternalError status code.
+// The Error() string of the error will be used as the response error message.
+// If the error implements ErrorCoder, the provided code will be set on the
+// response error.
+// If the error implements Headerer, the given headers will be set.
+func DefaultErrorEncoder(ctx context.Context, err error, w http.ResponseWriter) {
+
+	w.Header().Set("Content-Type", ContentType)
+
+	if headerer, ok := err.(httpTransport.Headerer); ok {
+		for k := range headerer.Headers() {
+			w.Header().Set(k, headerer.Headers().Get(k))
+		}
+	}
+
+	e := Error{
+		Code:    InternalError,
+		Message: err.Error(),
+	}
+
+	if sc, ok := err.(ErrorCoder); ok {
+		e.Code = sc.ErrorCode()
+	}
+
+	reqID, _ := ctx.Value(reqID).(*RequestID)
+
+	w.WriteHeader(http.StatusInternalServerError)
+	_ = json.NewEncoder(w).Encode(Response{
+		JSONRPC: Version,
+		Error:   &e,
+		ID:      reqID,
+	})
+}
+
+// ErrorCoder is checked by DefaultErrorEncoder. If an error value implements
+// ErrorCoder, the integer result of ErrorCode() will be used as the JSONRPC
+// error code when encoding the error.
+//
+// By default, InternalError (-32603) is used.
+type ErrorCoder interface {
+	ErrorCode() int
+}
+
+// interceptingWriter intercepts calls to WriteHeader, so that a finalizer
+// can be given the correct status code.
+type interceptingWriter struct {
+	http.ResponseWriter
+	code int
+}
+
+// WriteHeader may not be explicitly called, so care must be taken to
+// initialize w.code to its default value of http.StatusOK.
+func (w *interceptingWriter) WriteHeader(code int) {
+	w.code = code
+	w.ResponseWriter.WriteHeader(code)
+}
